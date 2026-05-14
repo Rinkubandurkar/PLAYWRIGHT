a04fb872273d4c54a0b529dc9caa2d83bec03f30
Rinkubandurkar <101648607+Rinkubandurkar@users.noreply.github.com>
2026-05-14 08:58:01 +0530

    Add let/const hosting and block scope examples

---
+++ Chapter_04_JavaScripts_concepts/21_Jr_QA.js
@@ -0,0 +1,11 @@
+
+// var  API_END_APP_VWD_COM ="httop://app.vwd.com/login/api"; need to be declared before using it
+
+console.log("API_END_APP_VWD");
+console.log("dasadf");
+
+if (true) {
+
+}
+
+var API_END_APP_VWD_COM = "httop://app.vwd.com/login/api";
